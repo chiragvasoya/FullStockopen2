d97943f05bf7bc5e59c935fc1467e126e2cb07b1
Chirag <chirag@ledworldlighting.com>
2022-12-08 16:23:31 -0700

    exercise step 2 of country data

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -9,6 +9,26 @@ const CountryList = ({countries, searchCount}) => {
             <div>Too many results. Further add to searchbox</div>
         )
     }
+    else if ( searchCount === 1) {
+       
+        return (
+            <div>
+                { countries.map(country => <>
+                 <h3>{country.name.common}</h3>
+                <p>Capital: {country.capital}</p>
+                <h3>Languages: </h3>
+                <ul>
+                    { 
+                    //   Object.keys(country.languages).map(langauge => <li key={langauge}>{country.languages[langauge]}</li>)
+                      Object.values(country.languages).map((langauge, i) => <li key={i}>{langauge}</li>)
+                    }
+                </ul> 
+                <img src={country.flags.png} /> 
+                </>
+                )}
+            </div>
+        )
+    }
     return (
         <ul>
         { countries.map(country => <li key={country.cca3}>{ country.name.common }</li> )  }
